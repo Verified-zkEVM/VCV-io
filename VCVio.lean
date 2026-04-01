9a5e3f8a3a5244cbf0b1898e7d703087a692c78a
Quang Dao <quang.dao@layerzerolabs.org>
2026-04-01 13:55:04 -0400

    chore: re-sort VCVio.lean imports after DataEncapMech rename

--- VCVio.lean
+++ VCVio.lean
@@ -7,6 +7,7 @@ import VCVio.CryptoFoundations.AsymmEncAlg.INDCPA.Oracle
 import VCVio.CryptoFoundations.Asymptotics.Negligible
 import VCVio.CryptoFoundations.Asymptotics.Security
 import VCVio.CryptoFoundations.CommitmentScheme
+import VCVio.CryptoFoundations.DataEncapMech
 import VCVio.CryptoFoundations.FiatShamir
 import VCVio.CryptoFoundations.FiatShamirWithAbort
 import VCVio.CryptoFoundations.Fischlin
@@ -22,7 +23,6 @@ import VCVio.CryptoFoundations.HardnessAssumptions.HardRelation
 import VCVio.CryptoFoundations.HardnessAssumptions.OneWay
 import VCVio.CryptoFoundations.IdenSchemeWithAbort
 import VCVio.CryptoFoundations.KEMDEM
-import VCVio.CryptoFoundations.DataEncapMech
 import VCVio.CryptoFoundations.KeyEncapMech
 import VCVio.CryptoFoundations.MacAlg
 import VCVio.CryptoFoundations.PRF
